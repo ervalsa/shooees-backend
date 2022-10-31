6cf629448f82af844ea5be6981050d10afce8437
ervalsa <ervalsa-san@gmail.com>
2022-10-31 09:13:57 +0800

    Add Middleware Admin Check

--- routes/web.php
+++ routes/web.php
@@ -17,12 +17,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified'
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
-});
+Route::middleware(['auth:sanctum', 'verified', 'admin'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
